9b8011ca817fa1810b60770f355780212fa59577
HP <vaibhavi.chaudhary1213@gmail.com>
2026-03-27 16:06:50 +0530

    Fix User model syntax error

--- models/User.js
+++ models/User.js
@@ -1,44 +1,88 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
-const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  profilePic: { type: String, default: "" },
-  bio: { type: String, default: "" },
-}, { timestamps: true });
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      minlength: 3,
+      maxlength: 30,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    bio: {
+      type: String,
+      maxlength: 150,
+      default: '',
+    },
+    profilePicture: {
+      type: String,
+      default: "https://res.cloudinary.com/demo/image/upload/v1/default-avatar.png",
+    },
+    coverPicture: {
+      type: String,
+      default: '',
+    },
+    followers: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    following: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    postsCount: {
+      type: Number,
+      default: 0,
+    },
+    isPrivate: {
+      type: Boolean,
+      default: false,
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    website: {
+      type: String,
+      trim: true,
+      default: '',
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-// Add these to your userSchema
-profilePicture: {
-  type: String,
-  default: "https://res.cloudinary.com/demo/image/upload/v1/default-avatar.png",
-},
-coverPicture: {
-  type: String,
-  default: "",
-},
-bio: {
-  type: String,
-  maxlength: 150,
-  default: "",
-},
-followers: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-following: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-postsCount: {
-  type: Number,
-  default: 0,
-},
-isPrivate: {
-  type: Boolean,
-  default: false,
-},
-isVerified: {
-  type: Boolean,
-  default: false,
-},
-website: {
-  type: String,
-  trim: true,
-},
+// Hash password before saving
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
 
-module.exports = mongoose.model("User", userSchema);
+// Compare password method
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
